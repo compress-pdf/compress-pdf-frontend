674363577485e9458f1e24e993e08c58da3a17a3
ayat1041 <ayat.dihan6593@gmail.com>
2025-01-27 17:01:07 +0600

    fix: update react-pdf version

--- src/components/pages/compress-pdf/home-page/index.tsx
+++ src/components/pages/compress-pdf/home-page/index.tsx
@@ -30,7 +30,8 @@ import AfterUpload from './UploadSection/AfterUpload';
 //   'pdfjs-dist/build/pdf.worker.min.mjs',
 //   import.meta.url
 // ).toString();
-pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
+// pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
+pdfjs.GlobalWorkerOptions.workerSrc = `//unpkg.com/pdfjs-dist@${pdfjs.version}/build/pdf.worker.min.mjs`;
 
 const HomePageContent = ({
   children,
